8a7f75d63036d809c53cc64ab1262b7b878b19de
Sheff3rd <thedoctorwhoovesmail@gmail.com>
2016-10-29 15:53:07 +0300

    Spec complete

--- ball.rb
+++ ball.rb
@@ -6,10 +6,14 @@ class Ball
   ANSWERS = YAML.load_file(File.join(__dir__, 'answers.yml'))
 
   def shake
-    rindex = rand(0..19)
+    rindex = rand(Ball::ANSWERS.length)
     answer = Ball::ANSWERS[rindex]
-    puts Ball::ANSWERS[rindex].to_s.colorize(31)
-    return answer
+    if rindex <= 4 then puts Ball::ANSWERS[rindex].to_s.colorize(31)
+    elsif rindex <= 9 then puts Ball::ANSWERS[rindex].to_s.colorize(32)
+    elsif rindex <= 14 then puts Ball::ANSWERS[rindex].to_s.colorize(33)
+    else puts Ball::ANSWERS[rindex].to_s.colorize(34)
+    end
+    answer
   end
 
 end
